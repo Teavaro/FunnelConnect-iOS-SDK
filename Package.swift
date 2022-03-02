3b35a36f70383b61128f1dcfe1054f6ff0d69632
Ahmad Mahmoud <amssm.eng@gmail.com>
2022-03-02 15:30:23 +0200

    Update.

--- Package.swift
+++ Package.swift
@@ -3,20 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "FunnelConnectSDK",
-    platforms: [
-        .iOS(.v12)
-    ],
-    products: [
-        .library(
-            name: "FunnelConnectSDK",
-            targets: ["FunnelConnectSDK"]
-        ),
-    ],
     targets: [
-        .binaryTarget(
-            name: "FunnelConnectSDK",
-            url: "https://github.com/Teavaro/FunnelConnectSDK_iOS/FunnelConnectSDK.zip",
-            checksum: "5e0052d153b078b250e03add159fc1d0e5ca5ed076cea6adf1f681afc27e67ac"
+        .target(
+            name: "FunnelConnectSDK"
         ),
     ]
 )
