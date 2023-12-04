7aac10b449949d66ffd44605ca8a4c5739990c5e
Ahmad Mahmoud <ams.eng@hotmail.com>
2023-12-04 17:03:49 +0200

    SPM Version 0.1.13

--- funnelConnectSDK.xcframework/ios-arm64/funnelConnectSDK.framework/Headers/funnelConnectSDK.h
+++ funnelConnectSDK.xcframework/ios-arm64/funnelConnectSDK.framework/Headers/funnelConnectSDK.h
@@ -339,7 +339,6 @@ __attribute__((swift_name("FunnelConnectSDK")))
 - (void)updatePermissionsPermissions:(FCSDKPermissions *)permissions dataCallback:(void (^)(NSString *))dataCallback errorCallback:(void (^)(NSError *))errorCallback __attribute__((swift_name("updatePermissions(permissions:dataCallback:errorCallback:)")));
 - (void)updatePermissionsPermissions:(FCSDKPermissions *)permissions errorCallback:(void (^)(NSError *))errorCallback __attribute__((swift_name("updatePermissions(permissions:errorCallback:)")));
 - (void)updatePermissionsPermissions:(FCSDKPermissions *)permissions notificationsName:(NSString *)notificationsName notificationsVersion:(int32_t)notificationsVersion dataCallback:(void (^)(NSString *))dataCallback errorCallback:(void (^)(NSError *))errorCallback __attribute__((swift_name("updatePermissions(permissions:notificationsName:notificationsVersion:dataCallback:errorCallback:)")));
-- (void)updatePermissionsPermissions:(FCSDKPermissions *)permissions notificationsName:(NSString *)notificationsName notificationsVersion:(int32_t)notificationsVersion errorCallback:(void (^)(NSError *))errorCallback __attribute__((swift_name("updatePermissions(permissions:notificationsName:notificationsVersion:errorCallback:)")));
 @end
 
 __attribute__((swift_name("Kotlinx_serialization_coreSerializationStrategy")))
